a8294b0d0fed66d55e44b69a7739ad4a5296c8b8
Dhileepguru <dhileepguru497@gmail.com>
2024-03-19 00:06:44 +0530

    updated media query(76px)

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  
+    window.addEventListener("scroll",function(){
+      var header=document.querySelector(".fixedheader");
+      var scrollPosition=window.scrollY;
+    
+      if(scrollPosition>0){
+        header.classList.add("scrolled");
+      }
+      else{
+        header.classList.remove("scrolled");
+      }
+    })
+    });
+    
